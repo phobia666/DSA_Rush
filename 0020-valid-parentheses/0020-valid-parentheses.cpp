98354c8287f7e7f19ce8eaa08b9d21f6ca68c4bf
Abhyanshu Shah <116498918+phobia666@users.noreply.github.com>
2026-07-10 16:22:41 +0530

    Time: 0 ms (100%), Space: 8.8 MB (67.84%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+bool isValid(string para) {
+    stack<char> st;
+
+    for (char c : para) {
+        if (c == '(' || c == '{' || c == '[') {
+            st.push(c);
+        } else {
+            if (st.empty()) return false;
+            char top = st.top();
+            st.pop();
+
+            if ((c == ')' && top != '(') ||
+                (c == '}' && top != '{') ||
+                (c == ']' && top != '[')) {
+                return false;
+            }
+        }
+    }
+    return st.empty();
+}
+};
