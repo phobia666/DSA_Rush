cf6491f29e9dad4490d1cebaf0b0dfd960fbbd5b
Abhyanshu Shah <116498918+phobia666@users.noreply.github.com>
2026-07-10 16:23:00 +0530

    Time: 1 ms (80.91%), Space: 12.3 MB (67.07%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        int num = 0;
+        int len = s.length();
+        vector<int> v(100);
+
+        for(int i = 0; i < len; i++){
+            switch(s[i]){
+                case 'I': v[i] = 1;
+                break;
+                case 'V': v[i] = 5;
+                break;
+                case 'X': v[i] = 10;
+                break;
+                case 'L': v[i] = 50;
+                break;
+                case 'C': v[i] = 100;
+                break;
+                case 'D': v[i] = 500;
+                break;
+                case 'M': v[i] = 1000;
+                break;
+                default: return -1;
+            }
+            if(i == 0 || v[i] <= v[i - 1]){
+                num += v[i];
+            }
+            else{
+                num += v[i] - 2 * v[i - 1];
+            }
+        }
+        return num;
+    }
+};
