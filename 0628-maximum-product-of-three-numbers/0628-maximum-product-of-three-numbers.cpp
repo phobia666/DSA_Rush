d80ef16a9333f029d84e2c4ff50a5db267bb2e4c
Abhyanshu Shah <116498918+phobia666@users.noreply.github.com>
2026-07-31 16:21:46 +0530

    Time: 0 ms (100%), Space: 31.4 MB (97.67%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
@@ -2,16 +2,43 @@ class Solution {
 public:
     int maximumProduct(vector<int>& a) {
         int n = a.size();
+        int first = INT_MIN;
+        int second = INT_MIN;
+        int third = INT_MIN;
+        int temp1;
+        int temp2;
 
-        sort(a.begin(), a.end());
+        int last = INT_MAX;
+        int lastSecond = INT_MAX;
+        int temp3;
 
-        int prod = 1;
-        if(n == 3){
-            return a[0] * a[1] * a[2];
+        for(int i = 0; i < n; i++){
+            if(a[i] > first) {
+                temp1 = first;
+                first = a[i];
+                temp2 = second;
+                second = temp1;
+                third = temp2;
+            }
+            else if(a[i] > second){
+                temp2 = second;
+                second = a[i];
+                third = temp2;
+            }
+            else if(a[i] > third){
+                third = a[i];
+            }
+
+            if(a[i] < last){
+                temp3 = last;
+                last = a[i];
+                lastSecond = temp3;
+            }
+            else if(a[i] < lastSecond){
+                lastSecond = a[i];
+            }
         }
-        prod = a[0] * a[1] * a[n - 1];
-        prod = max(prod, a[n - 1] * a[n - 2] * a[n - 3]);
 
-        return prod;
+        return max((first * second * third), (last * lastSecond * first)) ;
     }
 };
