ab844482f67ea023a15b90dea8bf76dd66424475
Abhyanshu Shah <116498918+phobia666@users.noreply.github.com>
2026-08-04 19:34:50 +0530

    Time: 5 ms (25.49%), Space: 33.7 MB (17.56%) - LeetHub

--- 3731-find-missing-elements/3731-find-missing-elements.cpp
+++ 3731-find-missing-elements/3731-find-missing-elements.cpp
@@ -2,21 +2,24 @@ class Solution {
 public:
     vector<int> findMissingElements(vector<int>& a) {
         int n = a.size();
+        unordered_set<int> st;
         vector<int> ans;
+        int small = INT_MAX;
+        int big = INT_MIN;
 
-        sort(a.begin(), a.end());
+        for(int i = 0; i < n; i++){
+            small = min(small, a[i]);
+            big = max(big, a[i]);
+            st.insert(a[i]);
+        }
 
-        for(int i = 1; i < n; i++){
-            int cnt = 1;
-            int diff = a[i] - a[i - 1];
-            int elem = a[i - 1] + 1;
-
-            while(cnt < diff){
-                ans.push_back(elem);
-                elem++;
-                cnt++;
+        for(int i = small + 1; i < big; i++){
+            if(st.find(i) == st.end()){
+                ans.push_back(i);
             }
         }
+
         return ans;
+
     }
 };
